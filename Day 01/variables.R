dc40015d90d3ae2a1c286d3ff32f27997fbc26e6
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-12-11 04:03:11 +0600

    Add files via upload

---
+++ Day 01/variables.R
@@ -0,0 +1,30 @@
+#variables
+
+x=10
+print(x) #simple number variable declare
+
+y='data science'
+print(y) #string variable declare 
+
+#underscore variable declare
+
+z_=40
+print(z_)
+print(class(z_)) #class is used to see the type of variable
+
+
+
+#assign function
+
+assign('x',60) #assign('variable name',value)
+print(x)
+
+
+#finding variable
+
+print(ls()) #ls() is used to see all the variables which is declaed
+
+#delete variables
+
+rm(x) ##rm() is used to delete any variable
+print(ls())
