04d5fc04ae722218516325c0cd49ef6ad0959f07
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2024-12-17 18:23:42 +0600

    Add files via upload

---
+++ Day 02/operators2.r
@@ -0,0 +1,36 @@
+#assignment opeartor 
+
+x=c(1,2,3) #left assignment
+y<-x
+print(x)
+print(y)
+
+
+x=c(1,2,3)  #right assignment
+y>-x
+print(x)
+print(y)
+
+
+#logical operator
+
+x=c(1,2,3) 
+y=c(1,3,5)
+print(x && y)
+
+
+x=c(1,2,3) 
+y=c(1,3,5)
+print(x || y)
+
+x=c(1,2,3) 
+y=c(1,3,5)
+print(x & y)
+
+x=c(1,2,3) 
+y=c(1,3,5)
+print(x | y)
+
+x=c(1,2,3) 
+y=c(1,3,5)
+print(!x)
